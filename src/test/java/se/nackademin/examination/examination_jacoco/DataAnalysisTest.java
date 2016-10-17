dee538dcbea33f1abc929c88f43d287e6bda3fe4
Lisa Bromark <lisabromark@yahoo.se>
2016-10-17 10:18:12 +0200

    this is my third commit

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -18,6 +18,48 @@ public class DataAnalysisTest {
 		assertTrue("The result should contain 'The name of the homecity is big and the participant is 30 or older' ",
 				result.contains("The name of the homecity is big and the participant is 30 or older"));
 
+		ArrayList<String> values2 = new ArrayList<String>();
+		values2.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "25", "Uppsala"));
+		String result2 = dataAnalysis.buildFinalString(values2);
+		assertTrue("The result should contain 'The name of the homecity is big and the participant is younger than 30' ",
+				result2.contains("The name of the homecity is big and the participant is younger than 30"));
+
+		ArrayList<String> values3 = new ArrayList<String>();
+		values3.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Rio"));
+		String result3 = dataAnalysis.buildFinalString(values3);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is 30 or older' ",
+				result3.contains("The name of the homecity is small and the participant is 30 or older"));
+
+		ArrayList<String> values4 = new ArrayList<String>();
+		values4.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "25", "Rio"));
+		String result4 = dataAnalysis.buildFinalString(values4);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is younger than 30' ",
+				result4.contains("The name of the homecity is small and the participant is younger than 30"));
+
+		ArrayList<String> values5 = new ArrayList<String>();
+		values5.addAll(Arrays.asList("Game", "Lisa", "Bromark", "F", "30", "Rio"));
+		String result5 = dataAnalysis.buildFinalString(values5);
+		assertTrue("The result should contain 'The first name is smaller or equals in size to the last name and the participant is 30 or younger' ",
+				result5.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+
+		ArrayList<String> values6 = new ArrayList<String>();
+		values6.addAll(Arrays.asList("Game", "Lisa", "Brom", "F", "30", "Rio"));
+		String result6 = dataAnalysis.buildFinalString(values6);
+		assertTrue("The result should contain 'The first name is smaller or equals in size to the last name and the participant is 30 or younger' ",
+				result6.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+
+		ArrayList<String> values7 = new ArrayList<String>();
+		values7.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "31", "Rio"));
+		String result7 = dataAnalysis.buildFinalString(values7);
+		assertTrue("The result should contain 'The first name is greater or equals in size to the last name and the participant older than 30' ",
+				result7.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+
+		ArrayList<String> values8 = new ArrayList<String>();
+		values8.addAll(Arrays.asList("Game", "Rafae", "Silva", "M", "31", "Rio"));
+		String result8 = dataAnalysis.buildFinalString(values8);
+		assertTrue("The result should contain 'The first name is greater or equals in size to the last name and the participant older than 30' ",
+				result8.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+
 	}
 
 }
--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -11,6 +11,13 @@ public class ResultsFromInputsTest {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
+		resultsFromInputs.setResultForAge(39);
+		assertEquals("The result should be 39", resultsFromInputs.getResultForAge(), 39);
+		resultsFromInputs.setResultForGender(1);
+		assertEquals("The result should be 1", resultsFromInputs.getResultForGender(), 1 );
+		resultsFromInputs.setResultForHomeCity(2);
+		assertEquals("The result should be 2", resultsFromInputs.getResultForHomeCity(), 2 );
+		
 	}
 
 }
