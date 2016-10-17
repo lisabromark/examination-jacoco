81875c86eeaf9f4e9af269e1b6cb75054d346e11
Lisa Bromark <lisabromark@yahoo.se>
2016-10-17 09:13:11 +0200

    this is my second commit

--- src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
@@ -11,6 +11,14 @@ public class ConversorTest {
 		Conversor conversor = new Conversor();
 		conversor.setNameConverterArrayValues();
 		assertEquals(conversor.getNameConverterArray().get(0), "Your first name is smaller than your last name");
+		conversor.setGenderConverterArrayValues();
+		assertEquals(conversor.getGenderConverterArray().get(0), "man!");
+		conversor.setAgeConverterArrayValues();
+		assertEquals(conversor.getAgeConverterArray().get(0),"You are young and talented! Things will come your way soon enough! ");
+		conversor.setCityConverterArrayValues();
+		assertEquals(conversor.getCityConverterArray().get(0), " A, ");
 	}
+	
+	
 
 }
